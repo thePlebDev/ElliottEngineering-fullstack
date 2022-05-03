7208e4faa087278f44149b8e8aa6e67038a45c18
Tristan Elliott <>
2022-05-03 15:18:40 -0300

    basic authentication and authorization

--- src/main/java/com/Elliott/Engineering/Website/Security/SecurityConfig.java
+++ src/main/java/com/Elliott/Engineering/Website/Security/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.Elliott.Engineering.Website.Security;
 
+import com.Elliott.Engineering.Website.Security.Filters.InitialAuthenticationFilter;
+import com.Elliott.Engineering.Website.Security.Filters.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     CustomAuthenticationProvider customAuthenticationProvider;
+
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth){
@@ -20,7 +29,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable();
-        http.cors().disable();
         http.authorizeRequests().anyRequest().permitAll();
+
+        http.addFilterAt(
+                new InitialAuthenticationFilter(authenticationManager()),
+                BasicAuthenticationFilter.class
+        );
+        http.addFilterAfter(jwtAuthenticationFilter,
+                BasicAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception{
+        return super.authenticationManager();
     }
 }
