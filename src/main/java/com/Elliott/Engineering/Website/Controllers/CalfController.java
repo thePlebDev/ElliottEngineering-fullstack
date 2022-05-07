46fe9cf1ed1c495ddba5b49fc090b4951f3d9a99
Tristan Elliott <>
2022-05-07 14:41:47 -0300

    adding controllers

---
+++ src/main/java/com/Elliott/Engineering/Website/Controllers/CalfController.java
@@ -0,0 +1,9 @@
+package com.Elliott.Engineering.Website.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/calf")
+public class CalfController {
+}
